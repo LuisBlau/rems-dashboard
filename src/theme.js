814af180cbfc80a608cd88820c167a67b8324352
Mukesh <mukesh.kafle@toshibagcs.com>
2022-08-12 14:46:56 -0500

    Fixed Theme, buttons and status flowing

--- src/theme.js
+++ src/theme.js
@@ -18,68 +18,9 @@ const theme = createTheme({
       default: '#f3f2f2',
     },
   },
-  components: {
-    // Override the main app bar.
-    MuiAppBar: {
-      styleOverrides: {
-        colorPrimary: {
-          backgroundColor: "#484848",
-          contrastText: '#e4e4e4',
-          // text color...
-          color: '#e4e4e4',
-        }
-      }
-    },
-    // Override sidebar blankspace.
-    MuiDrawer: {
-      styleOverrides: {
-        paper: {
-          backgroundColor: "#484848",
-          color: '#e4e4e4',
-        }
-      }
-    },
-    // Override side bar items. Not including buttons
-    MuiListItem: {
-      styleOverrides: {
-        root: {
-          background: "#484848",
-          color: '#e4e4e4',
-        }
-      }
-    },
-    // Override side bar buttons/icons.
-    MuiListItemIcon: {
-      styleOverrides: {
-        root: {
-          background: "#484848",
-          color: '#e4e4e4',
-        }
-      }
-    },
-    // Override buttons.
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          //button color
-          background:"#e4e4e4",
-          //text color
-          color: '#484848',
-        }
-      }
-    },
-    MuiIconButton: {
-      styleOverrides: {
-        root: {
-          //button color
-          background:"#e4e4e4",
-          //text color
-          color: '#484848',
-        }
-      }
-    },
-  }
+
 })
 
 export default theme;
+
 
