52e60fde7e14eeee87807ca519c2d1699b2a672d
Caleb Hobson <caleb.hobson@toshibagcs.com>
2022-10-04 21:20:21 +0000

    Fix Screenshot Functionality + Add 30 second Refresh

--- components/Connection/OverviewAgentPaper.js
+++ components/Connection/OverviewAgentPaper.js
@@ -11,7 +11,7 @@ import { Button, Grid, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import Modal from '@mui/material/Modal'
 import Box from '@mui/material/Box'
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Link from '@mui/material/Link'
 import UserContext from '../../pages/UserContext'
 
@@ -32,56 +32,6 @@ const Root = styled('div')((
   }
 }))
 
-function DisplayScreenShot (props) {
-  const [open, setOpen] = React.useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: '#f75777',
-    border: '2px solid #000',
-    outline: '#7c70b3',
-    boxShadow: 24,
-    p: 4
-  }
-  return (
-    <Grid item xs={12}>
-      <Button variant="contained" size="medium" onClick={handleOpen} >Screenshot</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-      >
-        <Box sx={style}>
-          <ScreenCapture data={props.data} />
-        </Box>
-      </Modal>
-    </Grid>
-  )
-}
-function ScreenCapture (props) {
-  const { data, error } = useSWR(
-    '/REMS/agentScreenShot?storeName=' + props.data.storeName + '&agentName=' + props.data.agentName
-    , fetcher)
-
-  if (error) return <div>No screenshot </div>
-  if (!data) return <div>loading...</div>
-  return (
-    <Root>
-      <Typography>
-        Screenshot
-      </Typography>
-      <Typography>
-        {data.last_updated}
-      </Typography>
-      <img className="card-img-top" src={'data:image/png;base64,' + data.image} width={300} height={300} alt="Card image cap" />
-    </Root>
-  )
-}
 function timeSince (date) {
   const seconds = ((new Date() - new Date(date)) / 1000)
 
@@ -173,6 +123,79 @@ function DisplaySalesApplication (props) {
   )
 }
 
+function ScreenshotModal ({ data, open, handleOpen, handleClose }) {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 625,
+    height: 625,
+    bgcolor: '#ffffff',
+    border: '2px solid #000',
+    outline: '#7c70b3',
+    boxShadow: 24,
+    p: 4
+  }
+  return (
+    <Grid item xs={12}>
+      <Button variant="contained" size="medium" onClick={handleOpen} >Screenshot</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+      >
+        <Box sx={style}>
+          <ScreenCaptureDisplay agentData={data} />
+        </Box>
+      </Modal>
+    </Grid>
+  )
+}
+
+function ScreenCaptureDisplay ({ agentData }) {
+  const screenCaptureCommand = { Retailer: agentData.retailer_id, Store: agentData.storeName, Agent: agentData.agentName, Command: 'ScreenCapture' }
+  const [screenshotData, setScreenshotData] = useState({})
+  fetch('/api/registers/commands/' + btoa(unescape(encodeURIComponent(JSON.stringify(screenCaptureCommand).replace('/\s\g', '')))))
+
+  const { data, error } = useSWR(
+    '/REMS/agentScreenShot?storeName=' + agentData.storeName + '&agentName=' + agentData.agentName
+    , fetcher, { refreshInterval: 30000 })
+
+  useEffect(() => {
+    if (error) return <div>No screenshot </div>
+    if (data === undefined || screenshotData === undefined) return <div>loading...</div>
+    if (data) {
+      if (data.image !== screenshotData.image || data.last_updated !== screenshotData.lastUpdated) {
+        setScreenshotData({ lastUpdated: data.last_updated, image: data.image })
+      }
+    }
+    const interval = setInterval(() => {
+      fetch('/api/registers/commands/' + btoa(unescape(encodeURIComponent(JSON.stringify(screenCaptureCommand).replace('/\s\g', '')))))
+
+      if (data.last_updated && data.image) {
+        if (data.image !== screenshotData.image || data.last_updated !== screenshotData.lastUpdated) {
+          setScreenshotData({ lastUpdated: data.last_updated, image: data.image })
+        }
+      }
+    }, 25000)
+
+    return () => clearInterval(interval)
+  })
+
+  return (
+    <Root>
+      <img className="card-img-top" src={'data:image/png;base64,' + screenshotData.image} width={600} height={600} alt="Gathering image data..." />
+      <Typography>
+        Last Updated: {screenshotData.lastUpdated}
+      </Typography>
+    </Root>
+  )
+}
+
 export default function OverviewAgentPaper (props) {
   const context = useContext(UserContext)
   let disableReload = true
@@ -180,12 +203,14 @@ export default function OverviewAgentPaper (props) {
     disableReload = false
   }
 
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   const jsonCommand = { Retailer: props.data.retailer_id, Store: props.data.storeName, Agent: props.data.agentName, Command: 'Reload' }
   const reload_link = 'javascript:fetch("/api/registers/commands/' + btoa(unescape(encodeURIComponent(JSON.stringify(jsonCommand).replace('/\s\g', '')))) + '")'
   jsonCommand.Command = 'Dump'
   const dump_link = 'javascript:fetch("/api/registers/commands/' + btoa(unescape(encodeURIComponent(JSON.stringify(jsonCommand).replace('/\s\g', '')))) + '")'
-  jsonCommand.Command = 'ScreenCapture'
-  const screencapture_link = 'javascript:fetch("/api/registers/commands/' + btoa(unescape(encodeURIComponent(JSON.stringify(jsonCommand).replace('/\s\g', '')))) + '")'
   jsonCommand.Command = 'Wake'
   const wake_link = 'javascript:fetch("/api/registers/commands/' + btoa(unescape(encodeURIComponent(JSON.stringify(jsonCommand).replace('/\s\g', '')))) + '")'
   jsonCommand.Command = 'Sleep'
@@ -285,7 +310,7 @@ export default function OverviewAgentPaper (props) {
       </Grid>
       <Grid container spacing={1}>
         <Grid item xs={12} sx={{ margin: 1 }}>
-          <DisplayScreenShot data={props.data} />
+          <ScreenshotModal data={props.data} open={open} handleOpen={handleOpen} handleClose={handleClose} />
         </Grid>
       </Grid>
     </Grid>
